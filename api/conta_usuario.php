1e5845d381475b9fad731ba155dbe032c8adac2f
Rycksz <rychardgabriel481@gmail.com>
2023-05-30 23:17:19 -0300

    adicionado os cards

--- api/conta_usuario.php
+++ api/conta_usuario.php
@@ -51,13 +51,62 @@ $logado = $_SESSION['email'];
                     <li><a href="#">carrinho</a></li>
                     <li><a href="sair.php">Sair</a></li>
                 </ul>
+
             </div>
 
             <?php
- 
-             echo "<h6>Bem Vindo <u>$logado</u></h6>";
+
+          echo "<h6>Bem Vindo <u>$logado</u></h6>";
 
 
-            ?>
+              ?>
+    
+</header>
+<!-- 
+     <div class="content">
+            <div class="textbox">
+                <h2>ESCOLHA SUA BMW</h2>
+            </div> -->
+
+       <div class="card">
+        <div>
+            <h1>BMW i8</h1>
+            <h2>elétrico</h2>
+            <span>R$ 100.000</span>
+            <button> COMPRAR </button>
+        </div>
+
+        <div class="card2">
+        <div>
+            <h1>BMW iX3</h1>
+            <h2>elétrico</h2>
+            <span>R$ 200.000</span>
+            <button> COMPRAR </button>
+        </div>
+
+        <div class="card3">
+        <div>
+            <h1>BMW X5</h1>
+            <h2>elétrico</h2>
+            <span>R$ 300.000</span>
+            <button> COMPRAR </button>
+        </div>
+
+        <div class="imgbox">
+                <img src="img/bmw01.png"  class="bmw01">
+            </div>
+
+            <div class="img2">
+                <img src="img/bmw2.png"  class="bmw02">
+            </div>
+
+            <div class="img3">
+                <img src="img/bmw3.png"  class="bmw03">
+            </div>
+       </div>  
+       
+      
+
+           
 </body>
 </html>
